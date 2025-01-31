7ba05ee149bc47c29001a7197b9e4aa1c5fdd7e1
ishfarmannan12 <128732221+ishfarmannan12@users.noreply.github.com>
2025-01-31 13:16:39 -0500

    Create HelloWorld.hpp

---
+++ HelloWorld.hpp
@@ -0,0 +1,12 @@
+// File: HelloWorld.hpp
+// Author: Ishfar Mannan
+// Date: 1/31/2025
+// A header file containing a function declaration for printing "Hello, World!"
+
+#ifndef HELLO_WORLD_HPP
+#define HELLO_WORLD_HPP
+
+// Declaration of the HelloWorld function.
+void HelloWorld();
+
+#endif // HELLO_WORLD_HPP
