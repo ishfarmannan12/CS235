cc2fe10af7a3ea99e95117484f54d8f2c1601c0a
ishfarmannan12 <128732221+ishfarmannan12@users.noreply.github.com>
2025-01-31 13:17:57 -0500

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+// File: HelloWorld.cpp
+// Author: Ishfar Mannan
+// Date: 1/31/2025
+// A source file that defines a function to print "Hello, World!"
+
+#include "HelloWorld.hpp"
+#include <iostream>
+
+// Definition of the HelloWorld function.
+void HelloWorld()
+{
+    std::cout << "Hello World" << std::endl;
+}
