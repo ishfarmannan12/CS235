6728efcace3a689537945a8cabcc5008197b37cb
ishfarmannan12 <128732221+ishfarmannan12@users.noreply.github.com>
2025-01-31 13:18:41 -0500

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,11 @@
+// File: test.cpp
+// Author: Ishfar Mannan
+// Date: 1/31/2025
+// A tes file thatt tests our function in HelloWorld.cpp
+
+#include "HelloWorld.hpp"
+
+int main() {
+    HelloWorld();
+    return 0;
+}
